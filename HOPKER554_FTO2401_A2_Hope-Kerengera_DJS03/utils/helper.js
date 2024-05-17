af26bad2644de0044cb9182847a13070c6ba10d6
Hope <k.hope.ing@gmail.com>
2024-05-17 14:37:01 +0200

    Added Documentation

--- HOPKER554_FTO2401_A2_Hope-Kerengera_DJS03/utils/helper.js
+++ HOPKER554_FTO2401_A2_Hope-Kerengera_DJS03/utils/helper.js
@@ -1,4 +1,9 @@
-//---FUNCTION TO CREATE PREVIEW BOOK ELEMENT
+/**
+ * FUNCTION TO CREATE PREVIEW BOOK ELEMENT
+ * @param {Object} book - The book object containing author, id, image and title.
+ * @param {Object} authors - The authors object.
+ * @returns {HTMLButtonElement} The created book element.
+ */
 export const createBookPreviewElement = (
   { author, id, image, title },
   authors
@@ -18,7 +23,13 @@ export const createBookPreviewElement = (
   return element;
 };
 
-//---FUNCTION TO CREATE OPTION FILTER ELEMENT
+/**
+ *FUNCTION TO CREATE OPTION FILTER ELEMENT
+ *
+ * @param {Object} options - The object containing the options.
+ * @param {string} defaultOptionText - The text to be displayed for the default option.
+ * @returns {DocumentFragment} - The created document fragment.
+ */
 export const createOptionElement = (options, defaultOptionText) => {
   const fragment = document.createDocumentFragment();
   const firstElement = document.createElement("option");
@@ -37,7 +48,11 @@ export const createOptionElement = (options, defaultOptionText) => {
   return fragment;
 };
 
-//---FUNCTION TO SET THEME
+/**
+ *FUNCTION TO SET THEME BASED ON THE PROVIDED THEME
+ *
+ * @param {string} theme - The theme to set the colours for different modes.
+ */
 export const setThemeColors = (theme) => {
   const colors = {
     darkColor: theme === "night" ? "255, 255, 255" : "10, 10, 20",
@@ -49,7 +64,7 @@ export const setThemeColors = (theme) => {
   document.documentElement.style.setProperty("--color-light", lightColor);
 };
 
-//---FUNCTION TO CHECK AND SET THEME
+//FUNCTION TO CHECK AND SET THEME
 export const checkAndSetTheme = () => {
   if (
     window.matchMedia &&
@@ -61,16 +76,22 @@ export const checkAndSetTheme = () => {
   }
 };
 
-//---FUNCTION TO FILTER BOOKS BASED ON CRITERIA---
+/**
+ * FUNCTION TO FILTER BOOKS BASED ON CRITERIA
+ *
+ * @param {Array} books - Array of book objects.
+ * @param {Object} filters - Object containing filters.
+ * @returns {Array} - Array of filtered book objects.
+ */
 export const filterBooks = (books, filters) => {
   return books.filter((book) => {
     const genreMatch =
-      filters.genre === "any" || book.genres.includes(filters.genre);
+      filters.genre === "any" || book.genres.includes(filters.genre); //filters.genre filters books by genre
     const titleMatch =
       filters.title.trim() === "" ||
-      book.title.toLowerCase().includes(filters.title.toLowerCase());
+      book.title.toLowerCase().includes(filters.title.toLowerCase()); //filters.title filters books by title
     const authorMatch =
-      filters.author === "any" || book.author === filters.author;
+      filters.author === "any" || book.author === filters.author; //filters.author filters books by author
 
     return genreMatch && titleMatch && authorMatch;
   });
